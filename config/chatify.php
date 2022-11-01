90c0216e609182ca2fb324980e93479da2a316c0
Ifan Lumape <71585876+ifanlumape85@users.noreply.github.com>
2022-11-01 12:18:56 +0800

    update

--- config/chatify.php
+++ config/chatify.php
@@ -6,7 +6,7 @@ return [
     | Messenger display name
     |-------------------------------------
     */
-    'name' => env('CHATIFY_NAME', 'Warkop Sipaten'),
+    'name' => env('CHATIFY_NAME', 'SITULAR PNS'),
 
     /*
     |-------------------------------------
